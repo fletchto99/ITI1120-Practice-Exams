d4e8d317a5ce768c83a6dfdf773c4e8a8bedab3f
Matthew Langlois <fletchto99@gmail.com>
2014-09-17 16:30:25 -0400

    Some 2D array questions from previous exams to sharpen up my memory on 2D arrays

---
+++ src/Exam2004.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+public class Exam2004 {
+
+    public static void main(String[] args) {
+        
+        /*
+         * The following code is the answer for question 5 off of http://mathieu.legault.me/resources/javacourse/CSI1100_F04_Exam.pdf
+         */
+        
+        // A magical array where the sum of all the rows (individually) and all of the columns (indivisually) add up to 15
+        int[][] testArray = new int[][] { { 2, 9, 4 }, { 7, 5, 3 }, { 6, 1, 8 } };
+
+        // print out the sub array in a list of values, not yet properly formatted (quick and dirty answer)
+        Arrays.stream(subMatrix(testArray, 1, 1))
+                .flatMapToInt(x -> Arrays.stream(x))
+                .forEach(System.out::println);
+
+        /*
+         * 
+         * Should give 5,3,1,8
+         * 
+         * If it was formatted properly it would look something like
+         * 
+         * {5,3}
+         * {1,8}
+         * 
+         * which would be the smaller 2d array (submatrix) of testArray
+         */
+
+    }
+
+    public static int[][] subMatrix(int[][] m, int row, int col) {
+
+        int[][] submatrix = new int[m.length - row][m[0].length - col];// create an array with the lengths of rows and columns of the submatrix
+
+        // start at the indicated row and increase the index
+        for (int i = row; i < m.length; i++) {
+            // start at the indicated column and increase the index
+            for (int j = col; j < m[i].length; j++) {
+                submatrix[i - row][j - col] = m[i][j];
+            }
+        }
+        return submatrix;
+    }
+
+}
---
+++ src/Exam2005.java
@@ -0,0 +1,47 @@
+import java.util.stream.IntStream;
+
+public class Exam2005 {
+
+    /*
+     * The following code is the answer for question 5 off of http://mathieu.legault.me/resources/javacourse/Exam2005.pdf
+     */
+
+    public static void main(String[] args) {
+        // A magical array where the sum of all the rows (individually) and all of the columns (indivisually) add up to 15
+        int[][] magicalArray = new int[][] { { 2, 9, 4 }, { 7, 5, 3 },
+                { 6, 1, 8 } };
+
+        // test if the array is magical
+        System.out.println(isMagic(magicalArray));
+    }
+
+    public static boolean isMagic(int[][] a) {
+        int[][] a_transpose = new int[a[0].length][a.length]; // The transposed array for summination of columns
+
+        int shouldEqual = IntStream.of(a[0]).sum(); // The sum of the first row, which all sums should equal
+
+        // Check the sum of all the rows in the 2d array
+        for (int i = 0; i < a.length; i++) {
+            if (IntStream.of(a[i]).sum() != shouldEqual) {// if the row doesn't equal the sum of the initial row then fail
+                return false;
+            }
+        }
+
+        // Ideally transpose would be another method which could be reused in other parts of code. But for simplicitiy's sake I added it directly into this method
+        // transpose the array, [i,j] index is now [j,i]
+        for (int i = 0; i < a[0].length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                a_transpose[i][j] = a[j][i];
+            }
+        }
+
+        // check the sum of all of the arrows in the transposed array
+        for (int i = 0; i < a.length; i++) {
+            if (IntStream.of(a[i]).sum() != shouldEqual) { // if the transposed column doesn't equal the sum then fail
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
